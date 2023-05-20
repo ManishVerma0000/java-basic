357891e28d914db9aafb15b1a0a69a0b975595c1
Dipali <dipali.nexfytech@gmail.com>
2023-05-20 15:50:26 +0530

    find the leap year

---
+++ leapYearfind.java
@@ -0,0 +1,22 @@
+public class leapYearfind {
+
+    public static void main(String[] args) {
+        int year = 2016;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("this is the leap year");
+                } else {
+                    System.out.println("not the leap year");
+                }
+            } else {
+                System.out.println("this is the leap year");
+            }
+
+        } else {
+            System.out.println("this year is not a leap year");
+        }
+
+    }
+
+}
---
+++ odd.java
@@ -0,0 +1,16 @@
+
+//check the numbr is odd
+
+public class odd {
+
+    public static void main(String[] args) {
+        int a = 11;
+        if (a % 2 == 0) {
+            System.out.println("number is even");
+
+        } else {
+            System.out.println("number is odd");
+        }
+
+    }
+}
